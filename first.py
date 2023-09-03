1b8ec228467c2cfaff831fba999b4c6d918cd111
kamal <kamal>
2023-09-03 23:56:31 +0530

    fruits added

--- first.py
+++ first.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 streamlit.title('Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -6,5 +8,9 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
 
 
